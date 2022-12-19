2db4a734a6b35cb15a3156407fe0b3e187555c1e
Filippo Sbaiz <sbaizfilippo@gmail.com>
2022-12-19 17:42:51 +0100

    next and prev

--- js/script.js
+++ js/script.js
@@ -37,6 +37,17 @@ methods: {
     changeImage(index){
         this.activeImage = index;
     },
-    
+    next(){
+        this.activeImage++;
+        if(this.activeImage > this.games.image.length -1){
+            this.activeImage = 0;
+        }
+    },
+    prev(){
+        this.activeImage--;
+        if(this.activeImage < 0){
+            this.activeImage = this.games.image.length -1;
+        }
+    },
 },
 }).mount('#app')
